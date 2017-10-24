ba9eb5643dfdc0d5d89e0d5fd059d1c638d4a247
SzeMTan <SzeMTan>
2017-10-24 16:22:41 +1300

    comment on quitplayController

--- src/tatai/view/QuitPlayConfirmController.java
+++ src/tatai/view/QuitPlayConfirmController.java
@@ -3,6 +3,13 @@ package tatai.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+/**
+ * This controller belongs to the fxml pop-up for when the user wants to exit the Main window. It asks
+ * for confirmation to exit, "Yes" will save all the data into JSON objects and "No" will just close the curret
+ * pop-up and continue the application.
+ * @author se206
+ *
+ */
 public class QuitPlayConfirmController {
 
 	@FXML private Button _yesBtn;
